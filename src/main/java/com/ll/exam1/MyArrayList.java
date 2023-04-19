f4aaad00a20b94d75660495d5636c052565de251
eoeo2255 <naiga6@gmail.com>
2023-04-19 15:01:19 +0900

    기본 메서드 구현

--- src/main/java/com/ll/exam1/MyArrayList.java
+++ src/main/java/com/ll/exam1/MyArrayList.java
@@ -1,13 +1,21 @@
 package com.ll.exam1;
 
 public class MyArrayList<T> {
+    private String[] data = new String[3];
+    int size =0;
 
-    public void add(T element1) {
+    public boolean add(String element) {
+        data[size] = element;
+        size++;
 
+        return true;
     }
 
     public int size() {
-        return 3;
+        return size;
     }
 
+    public String get(int i) {
+        return data[i];
+    }
 }
--- src/test/java/com/ll/exam1/MyArrayListTest.java
+++ src/test/java/com/ll/exam1/MyArrayListTest.java
@@ -1,25 +1,31 @@
 package com.ll.exam1;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 class MyArrayListTest {
-
-    private MyArrayList<String> list;
-
-    @BeforeEach
-    void setUp() {
-        list = new MyArrayList<>();
-    }
-
     @Test
     void testSize() {
+        MyArrayList<String> list = new MyArrayList<>();
+
         list.add("Element1");
         list.add("Element2");
         list.add("Element3");
-        assertEquals(3, list.size());
+
+        assertThat(list.size()).isEqualTo(3);
+    }
+
+    @Test
+    void testAdd() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("Element1");
+
+        assertThat(list.size()).isEqualTo(1);
+        assertThat(list.get(0)).isEqualTo("Element1");
     }
 
 }
