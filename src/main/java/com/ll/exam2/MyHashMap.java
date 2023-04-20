d38887a7faa7fe990fa156f52c284a97761bd95c
eoeo2255 <naiga6@gmail.com>
2023-04-20 13:42:09 +0900

    get() 구현

--- src/main/java/com/ll/exam2/MyHashMap.java
+++ src/main/java/com/ll/exam2/MyHashMap.java
@@ -2,12 +2,44 @@ package com.ll.exam2;
 
 public class MyHashMap<K, V> {
     private int size = 0;
+    private final Object[] keys;
+    private final Object[] values;
+
+    public MyHashMap() {
+        this(3);
+    }
+
+    public MyHashMap(int arrayLength) {
+        keys = new Object[arrayLength];
+        values = new Object[arrayLength];
+    }
 
     public int size() {
         return size;
     }
 
-    public void put(K key, V value) {
+    public V put(K key, V value) {  // map >> put을 했을 때 덮어쓰기가 되면 덮어씌워지기 이전 값을 반환한다.
+        keys[size] = key;
+        values[size] = value;
+
         size++;
+        return null;
+    }
+
+    public V get(K key) {
+        int indexOfKey = indexOfKey(key);
+
+        if (indexOfKey == -1) {return null;}
+
+        return (V) values[indexOfKey];
+    }
+
+    private int indexOfKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if(key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- src/test/java/com/ll/exam/MyHashMapTest.java
+++ src/test/java/com/ll/exam/MyHashMapTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.in;
 
 public class MyHashMapTest {
 
@@ -17,12 +18,30 @@ public class MyHashMapTest {
 
     @Test
     @DisplayName("put()")
-    void t0() {
+    void t02() {
         MyHashMap<String, Integer> map = new MyHashMap<>();
 
         map.put("철수", 24);
         map.put("영희", 17);
 
         assertThat(map.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("get()")
+    void t03() {
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+
+        map.put("철수", 24);
+        map.put("영희", 17);
+        map.put("울트론", 183118797);
+
+        int ageOf철수 = map.get("철수");
+        int ageOf영희 = map.get("영희");
+        int ageOf울트론 = map.get("울트론");
+
+        assertThat(ageOf철수).isEqualTo(24);
+        assertThat(ageOf영희).isEqualTo(17);
+        assertThat(ageOf울트론).isEqualTo(183118797);
     }
 }
